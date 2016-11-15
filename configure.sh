0338c61ed792374a0e4989a3851f4cda1208021c
bunjee <bunjee@omega.gg>
2016-11-15 20:51:27 +0100

    Configure: Fix syntax

--- configure.sh
+++ configure.sh
@@ -54,8 +54,6 @@ fi
 # VLC
 #--------------------------------------------------------------------------------------------------
 
-echo "COPYING VLC"
-
 if [ $1 = "qt4" ]; then
 
     bin="$bin4"
@@ -64,6 +62,8 @@ else
 fi
 
 if [ $2 = "win32" ]; then
+
+    echo "COPYING VLC"
 
     rm -rf "$bin"/plugins
     mkdir  "$bin"/plugins

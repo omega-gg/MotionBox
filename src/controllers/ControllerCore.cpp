8153060849fc1a01d4369138171aa09229abf655
bunjee <bunjee@omega.gg>
2019-05-31 10:20:56 +0200

    MotionBox 1.5.0-3

--- src/controllers/ControllerCore.cpp
+++ src/controllers/ControllerCore.cpp
@@ -73,7 +73,7 @@ W_INIT_CONTROLLER(ControllerCore)
 //-------------------------------------------------------------------------------------------------
 // Static variables
 
-static const QString CORE_VERSION = "1.5.0-2";
+static const QString CORE_VERSION = "1.5.0-3";
 
 static const QString PATH_SK = "../../Sky/src";
 

56a135c4945b59511900d39393447291a546e7fe
bunjee <bunjee@omega.gg>
2020-05-08 17:15:04 +0200

    ControllerCore: Update QML declarations

--- src/controllers/ControllerCore.cpp
+++ src/controllers/ControllerCore.cpp
@@ -34,6 +34,7 @@
 #include <QStandardPaths>
 #endif
 #endif
+#include <QAbstractItemModel>
 
 // Sk includes
 #include <WControllerApplication>
@@ -45,17 +46,44 @@
 #include <WControllerPlaylist>
 #include <WControllerMedia>
 #include <WControllerTorrent>
+#include <WView>
+#include <WViewResizer>
+#include <WViewDrag>
 #include <WWindow>
 #include <WCache>
+#include <WActionCue>
+#include <WInputCue>
 #include <WLoaderNetwork>
 //#include <WLoaderWeb>
 #include <WLoaderTorrent>
 #include <WHookTorrent>
 #include <WLibraryFolderRelated>
+#include <WAbstractTabs>
+#include <WAbstractTab>
 #include <WTabsTrack>
 #include <WTabTrack>
 #include <WBackendIndex>
 #include <WBackendUniversal>
+#include <WModelList>
+#include <WModelLibraryFolder>
+#include <WModelPlaylist>
+#include <WModelRange>
+#include <WModelCompletionGoogle>
+#include <WModelContextual>
+#include <WModelTabs>
+#include <WImageColorFilter>
+#include <WDeclarativeApplication>
+#include <WDeclarativeMouseArea>
+#include <WDeclarativeMouseWatcher>
+#include <WDeclarativeListView>
+#include <WDeclarativeContextualPage>
+#include <WDeclarativeAnimated>
+#include <WDeclarativeBorders>
+#include <WDeclarativeImage>
+#include <WDeclarativeImageSvg>
+#include <WDeclarativeBorderImage>
+#include <WDeclarativeTextSvg>
+#include <WDeclarativePlayer>
 
 // Application includes
 #include "DataLocal.h"
@@ -189,11 +217,159 @@ ControllerCore::ControllerCore() : WController()
 
     //---------------------------------------------------------------------------------------------
     // QML
+    //---------------------------------------------------------------------------------------------
+    // Qt
+
+    qmlRegisterUncreatableType<QAbstractItemModel>("Sky", 1,0, "QAbstractItemModel",
+                                                   "QAbstractItemModel is abstract");
+
+    //---------------------------------------------------------------------------------------------
+    // Global
+
+    qmlRegisterUncreatableType<WControllerDeclarative>("Sky", 1,0, "Sk", "Sk is not creatable");
+
+    //---------------------------------------------------------------------------------------------
+    // Applications
+
+    qmlRegisterType<WDeclarativeApplication>("Sky", 1,0, "Application");
+
+    //---------------------------------------------------------------------------------------------
+    // Kernel
+
+    qmlRegisterUncreatableType<WAbstractTabs>("Sky", 1,0, "AbstractTabs",
+                                              "AbstractTabs is abstract");
+
+    qmlRegisterUncreatableType<WAbstractTab>("Sky", 1,0, "AbstractTab",
+                                             "AbstractTab is abstract");
+
+    qmlRegisterType<WActionCue>("Sky", 1,0, "ActionCue");
+    qmlRegisterType<WInputCue> ("Sky", 1,0, "InputCue");
+
+    //---------------------------------------------------------------------------------------------
+    // View
+
+    qmlRegisterUncreatableType<WView>("Sky", 1,0, "View", "View is abstract");
+
+    qmlRegisterType<WViewResizer>("Sky", 1,0, "ViewResizer");
+    qmlRegisterType<WViewDrag>   ("Sky", 1,0, "ViewDrag");
+
+    qmlRegisterType<WWindow>("Sky", 1,0, "BaseWindow");
+
+    //---------------------------------------------------------------------------------------------
+    // Declarative
+
+    qmlRegisterType<WDeclarativeMouseArea>   ("Sky", 1,0, "MouseArea");
+    qmlRegisterType<WDeclarativeMouseWatcher>("Sky", 1,0, "MouseWatcher");
+    qmlRegisterType<WDeclarativeDrag>        ("Sky", 1,0, "Drag");
+
+    qmlRegisterType<WDeclarativeListHorizontal>("Sky", 1,0, "ListHorizontal");
+    qmlRegisterType<WDeclarativeListVertical>  ("Sky", 1,0, "ListVertical");
+
+    qmlRegisterType<WDeclarativeContextualPage>("Sky", 1,0, "ContextualPage");
+
+    qmlRegisterType<WDeclarativeAnimated>("Sky", 1,0, "Animated");
+
+    qmlRegisterType<WDeclarativeBorders>("Sky", 1,0, "Borders");
+
+    qmlRegisterUncreatableType<WDeclarativeImageBase>("Sky", 1,0, "ImageBase",
+                                                      "ImageBase is abstract");
+
+    qmlRegisterType<WDeclarativeImage>     ("Sky", 1,0, "Image");
+    qmlRegisterType<WDeclarativeImageScale>("Sky", 1,0, "ImageScale");
+    qmlRegisterType<WDeclarativeImageSvg>  ("Sky", 1,0, "ImageSvg");
+
+#ifdef QT_4
+    qmlRegisterType<WDeclarativeImageSvgScale>("Sky", 1,0, "ImageSvgScale");
+#endif
+
+    qmlRegisterType<WDeclarativeBorderImage>     ("Sky", 1,0, "BorderImage");
+    qmlRegisterType<WDeclarativeBorderImageScale>("Sky", 1,0, "BorderImageScale");
+    qmlRegisterType<WDeclarativeBorderGrid>      ("Sky", 1,0, "BorderGrid");
+
+    qmlRegisterType<WDeclarativeTextSvg>("Sky", 1,0, "TextSvg");
+
+#ifdef QT_4
+    qmlRegisterType<WDeclarativeTextSvgScale>("Sky", 1,0, "TextSvgScale");
+#endif
+
+    qmlRegisterType<WDeclarativePlayer>("Sky", 1,0, "Player");
+
+    //---------------------------------------------------------------------------------------------
+    // Models
+
+    qmlRegisterType<WModelList>("Sky", 1,0, "ModelList");
+
+    qmlRegisterType<WModelLibraryFolder>        ("Sky", 1,0, "ModelLibraryFolder");
+    qmlRegisterType<WModelLibraryFolderFiltered>("Sky", 1,0, "ModelLibraryFolderFiltered");
+
+    qmlRegisterType<WModelPlaylist>("Sky", 1,0, "ModelPlaylist");
+
+    qmlRegisterType<WModelRange>("Sky", 1,0, "ModelRange");
+
+    qmlRegisterType<WModelCompletionGoogle>("Sky", 1,0, "ModelCompletionGoogle");
+
+    qmlRegisterType<WModelContextual>("Sky", 1,0, "ModelContextual");
+
+    qmlRegisterType<WModelTabs>("Sky", 1,0, "ModelTabs");
+
+    //---------------------------------------------------------------------------------------------
+    // Multimedia
+
+    qmlRegisterUncreatableType<WBackendNet>("Sky", 1,0, "BackendNet", "BackendNet is abstract");
+
+    qmlRegisterUncreatableType<WAbstractBackend>("Sky", 1,0, "AbstractBackend",
+                                                 "AbstractBackend is abstract");
+
+    qmlRegisterUncreatableType<WAbstractHook>("Sky", 1,0, "AbstractHook",
+                                              "AbstractHook is abstract");
+
+    qmlRegisterUncreatableType<WLocalObject>("Sky", 1,0, "LocalObject", "LocalObject is abstract");
+
+    qmlRegisterUncreatableType<WLibraryItem>("Sky", 1,0, "LibraryItem", "LibraryItem is abstract");
+
+    qmlRegisterType<WLibraryFolder>       ("Sky", 1,0, "LibraryFolder");
+    qmlRegisterType<WLibraryFolderRelated>("Sky", 1,0, "LibraryFolderRelated");
+
+    qmlRegisterType<WPlaylist>("Sky", 1,0, "Playlist");
+
+    qmlRegisterType<WTabTrack> ("Sky", 1,0, "TabTrack");
+    qmlRegisterType<WTabsTrack>("Sky", 1,0, "BaseTabsTrack");
+
+    //---------------------------------------------------------------------------------------------
+    // Image
+
+    qmlRegisterUncreatableType<WImageFilter>("Sky", 1,0, "ImageFilter", "ImageFilter is abstract");
+
+    qmlRegisterType<WImageColorFilter>("Sky", 1,0, "ImageColorFilter");
+
+    qmlRegisterType<WDeclarativeGradient>    ("Sky", 1,0, "ScaleGradient");
+    qmlRegisterType<WDeclarativeGradientStop>("Sky", 1,0, "ScaleGradientStop");
+
+    //---------------------------------------------------------------------------------------------
+    // Events
+
+    qmlRegisterUncreatableType<WDeclarativeMouseEvent>("Sky", 1,0, "DeclarativeMouseEvent",
+                                                       "DeclarativeMouseEvent is not creatable");
+
+    qmlRegisterUncreatableType<WDeclarativeDropEvent>("Sky", 1,0, "DeclarativeDropEvent",
+                                                      "DeclarativeDropEvent is not creatable");
+
+    qmlRegisterUncreatableType<WDeclarativeKeyEvent>("Sky", 1,0, "DeclarativeKeyEvent",
+                                                     "DeclarativeKeyEvent is not creatable");
+
+    //---------------------------------------------------------------------------------------------
+    // MotionBox
 
     qmlRegisterType<DataLocal>("Sky", 1,0, "DataLocal");
 
-    wControllerDeclarative->setContextProperty("core", this);
+    //---------------------------------------------------------------------------------------------
+    // Context
 
+    wControllerDeclarative->setContextProperty("sk",                sk);
+    wControllerDeclarative->setContextProperty("controllerFile",    wControllerFile);
+    wControllerDeclarative->setContextProperty("controllerNetwork", wControllerNetwork);
+
+    wControllerDeclarative->setContextProperty("core",  this);
     wControllerDeclarative->setContextProperty("local", _local);
 }
 

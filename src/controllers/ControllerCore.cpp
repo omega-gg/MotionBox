8181169e51bea774e3bb3effab70892043d2a760
bunjee <bunjee@omega.gg>
2020-10-13 15:44:36 +0200

    MotionBox 1.6.0-7

--- src/controllers/ControllerCore.cpp
+++ src/controllers/ControllerCore.cpp
@@ -93,7 +93,7 @@ W_INIT_CONTROLLER(ControllerCore)
 //-------------------------------------------------------------------------------------------------
 // Static variables
 
-static const QString CORE_VERSION = "1.6.0-6";
+static const QString CORE_VERSION = "1.6.0-7";
 
 static const int LOG_LENGTH = 4000;
 

a4e42c223ebefeeca891ffe7a1e1f7b8319beed3
bunjee <bunjee@omega.gg>
2022-11-10 16:12:29 +0100

    android: Update to NDK 23

--- build.sh
+++ build.sh
@@ -44,7 +44,7 @@ WindowsKit_version="10"
 SDK_version="31"
 SDK_version_minimum="21"
 
-NDK_version="21"
+NDK_version="23"
 
 #--------------------------------------------------------------------------------------------------
 # environment

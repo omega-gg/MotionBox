f085bdd28f674400ef76c2878c2359e2b2e3bd58
bunjee <bunjee@omega.gg>
2025-03-26 10:41:31 +0100

    Qt 6.8.1 (10)

--- build.sh
+++ build.sh
@@ -42,9 +42,7 @@ WindowsKit_version="10"
 # Android
 
 SDK_version="34"
-SDK_version_minimum="21"
-
-NDK_version="26"
+SDK_version_minimum="24"
 
 #--------------------------------------------------------------------------------------------------
 # environment
@@ -310,9 +308,9 @@ elif [ $1 = "android" ]; then
 
     spec=android-clang
 
-    export ANDROID_NDK_ROOT="$external/NDK/$NDK_version"
+    export ANDROID_NDK_ROOT="$external/NDK/default"
 
-    export ANDROID_NDK_PLATFORM="android-$SDK_version"
+    export ANDROID_NDK_PLATFORM="android-$SDK_version_minimum"
 
     # NOTE android: This variable enforces the linux clang compiler.
     export ANDROID_NDK_HOST="linux-x86_64"
